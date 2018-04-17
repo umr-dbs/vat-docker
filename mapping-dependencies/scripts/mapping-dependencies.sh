3e61fdcd4b37cb7cb100f6b7e15c71c3fd0445af
Christian Beilschmidt <fyaa@ntacity.de>
2018-04-17 17:54:05 +0200

    added support for Clang 5.0

--- mapping-dependencies/scripts/mapping-dependencies.sh
+++ mapping-dependencies/scripts/mapping-dependencies.sh
@@ -8,15 +8,18 @@ set -x # Output command
 add-apt-repository ppa:ubuntugis/ppa
 ## Poco
 add-apt-repository ppa:gezakovacs/poco
-# CMake >= 3.7
-#add-apt-repository ppa:adrozdoff/cmake
+## CMake >= 3.7
+# add-apt-repository ppa:adrozdoff/cmake
+## CLang 5.0
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
+apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-5.0 main"
 
 apt-get update
 
 # Programs
 ## Build tools
 apt-get install --yes \
-    clang \
+    clang-5.0 \
     curl \
     git \
     make \
